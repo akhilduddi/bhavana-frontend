33c4151691c73f644330a8ecf2c1ae20754ab744
Akhil Duddi <akhilduddi95@gmail.com>
2025-08-21 22:42:45 +0530

    api added

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:7800',
+        target: 'https://bhavana-backend.onrender.com',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
